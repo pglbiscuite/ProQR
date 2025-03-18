384ddbe4743a03139fa16a9be9b65b3092310cee
pglbiscuite <114862250+pglbiscuite@users.noreply.github.com>
2025-03-18 12:42:07 +0200

    Version 0.0.1 - Implemented The QR Generation Functionallity in Memory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 import qrcode
+from io import BytesIO
+from base64 import b64encode
+
 
 app = Flask(__name__)
 
@@ -12,12 +15,19 @@ def home():
 
 
 
-#   Inserting The Link Function
+#   Inserting The Link Function & Return It
 @app.route('/', methods=['POST'])
 def generateQR():
+
+    memory = BytesIO()
     data = request.form.get('link')
     img = qrcode.make(data)
+    img.save(memory)
+    memory.seek(0)
 
+    base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode('ascii')
+
+    return render_template('index.html', data=base64_img)
 
 
 
